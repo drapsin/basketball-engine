d904690e663723f0dcb79dd4946f34f2decab3f7
Uros Drapsin <uros.drapsin@gmail.com>
2026-08-06 17:57:26 +0200

    Introduced Image service

---
+++ nba app/Services/Image/LocalImageUploader.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace nba_mvc.Services.Image
+{
+    public class LocalImageUploader : IImageUploader
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public LocalImageUploader(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        public async Task<string> UploadImageAsync(IFormFile image)
+        {
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+            var folderPath = Path.Combine(_env.WebRootPath, "image");
+
+            Directory.CreateDirectory(folderPath);
+
+            var savePath = Path.Combine(folderPath, fileName);
+            using (var stream = new FileStream(savePath, FileMode.Create))
+            {
+                await image.CopyToAsync(stream);
+            }
+
+            return "/image/" + fileName;
+        }
+    }
+}
